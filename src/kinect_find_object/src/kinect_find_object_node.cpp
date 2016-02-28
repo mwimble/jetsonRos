499ff6b284a6f70892ade1cd58dba17cf69fe3ff
Michael Wimble <mike@webself.com>
2016-02-28 04:43:50 +0000

    added countour detection

--- src/kinect_find_object/src/kinect_find_object_node.cpp
+++ src/kinect_find_object/src/kinect_find_object_node.cpp
@@ -6,6 +6,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
@@ -20,6 +22,9 @@ private:
 
 	int iLowV;
 	int iHighV;
+
+	int contourSizeThreshold;
+
 
 	static const string OPENCV_WINDOW;
 
@@ -53,9 +58,79 @@ private:
 		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
 		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
-		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		vector<int> smallBlobs;
+		double contourSize;
+		Mat tempImage;
+
+		imgThresholded.copyTo(tempImage);
+		findContours(tempImage, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+		vector<Rect> boundRect( contours.size() );
+		vector<vector<Point> > contours_poly( contours.size() );
+		vector<Point2f>center( contours.size() );
+		vector<float>radius( contours.size() );
+
+		if (!contours.empty()) {
+			for (size_t i = 0; i < contours.size(); i++) {
+				contourSize = contourArea(contours[i]);
+				if (contourSize > contourSizeThreshold) {
+					approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+					boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+					minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+       				smallBlobs.push_back(i);//#####
+				}
+			}
+
+			// for (size_t i = 0; i < smallBlobs.size(); i++) {
+			// 	Scalar color = Scalar(rand() % 255, rand() % 255, rand() % 255);
+			// 	drawContours(cv_ptr->image, contours, smallBlobs[i], color, CV_FILLED, 8);
+			// }
+			for (size_t i = 0; i < contours.size(); i++) {
+				double area = contourArea(contours[i]);
+				if (area < 100) continue;
+
+				double x = center[i].x;
+				double y = center[i].y;
+
+				int cols = cv_ptr->image.cols;
+				int rows = cv_ptr->image.rows;
+
+				string lr;
+				string fb;
+
+				if (x < int(cols * 0.33)) {
+					lr = "FAR LEFT";
+				} else if (x > int(cols * 0.66)) {
+					lr = "FAR RIGHT";
+				} else if (x < int(cols * 0.47)) {
+					lr = "LEFT";
+				} else if (x > int(cols * 0.53)) {
+					lr = "RIGHT";
+				} else {
+					lr = "LR OK";
+				}
+
+				if (y < int(rows * 0.33)) {
+					fb = "VERY FAR AWAY";
+				} else if (y < int(rows * 0.5)) {
+					fb = "FAR AWAY";
+				} else if (y < int(rows * 0.66)) {
+					fb = "NEAR";
+				} else {
+					fb = "VERY NEAR";
+				}
+
+				Scalar color = Scalar(rand() % 255, rand() % 255, rand() % 255);
+				circle(cv_ptr->image, center[i], (int)radius[i], color, 2, 8, 0 );
+				ROS_INFO("i: %d, area: %f, center: %f, %f, lr: %s, fb: %s", i, area, x, y, lr.c_str(), fb.c_str());
+			}
+		}
+
 		imshow(OPENCV_WINDOW, cv_ptr->image); //show the original image
-		cv::waitKey(3);
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
+		cv::waitKey(1);
 		//cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 
     }
@@ -73,10 +148,11 @@ public:
 	FindObject() : it_(nh_) {
 		iLowH = 116;
 		iHighH = 134;
-		iLowS = 96;
+		iLowS = 50;
 		iHighS = 255;
 		iLowV = 0;
 		iHighV = 255;
+		contourSizeThreshold = 100;
 
 		ros::param::param<std::string>("image_topic_name", imageTopicName_, "/camera/rgb/image_color");
 		ROS_INFO("PARAM image_topic_name: %s", imageTopicName_.c_str());
@@ -94,6 +170,7 @@ public:
 		cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
 		cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
+		cvCreateTrackbar("contourSizeThreshold", "Control", &contourSizeThreshold, 10000);
 	}
 };
 
